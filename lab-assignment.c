9e6da82003b61fa022239326962382ee8fa2295f
zubayer <zubayer3570@gmail.com>
2022-09-16 19:01:04 +0600

    first

---
+++ lab-assignment.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int event_count, events[100], u_demon = 0, angle = 0;
+    printf("Enter the number of events: ");
+    scanf("%d", &event_count);
+    int temp = event_count;
+    while (event_count--)
+    {
+        scanf("%d", &events[event_count]);
+    }
+    while (temp--)
+    {
+        events[temp] > 0 ? 
+        angle += events[temp] 
+        : 
+        angle ? angle-- : u_demon++;
+    }
+
+    printf("Undefeated demon: %d\n", u_demon);
+}
